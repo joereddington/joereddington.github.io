758013384cb70d6ed329ef835eb2ecc7b4c810ec
joe.reddington@gmail.com <joe@joereddington.com>
2023-09-19 15:25:58 +0100

    Added a script for making sure all the images are added

---
+++ add_missing_images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd "$( dirname "${BASH_SOURCE[0]}"
+
+# Check if the markdown file argument is provided
+if [ $# -eq 0 ]; then
+  echo "Please provide the markdown file name as an argument."
+  exit 1
+fi
+
+# Markdown file path
+markdown_file="$1"
+
+# Extract image names from markdown file using sed
+
+image_names=$(sed -n 's/.*!\[.*\](\([^)]*\)).*/\1/p' $markdown_file)
+echo $image_names
+# Iterate over each image name and stage the files
+for image_name in $image_names; do
+  image_path="$(pwd)/$image_name"
+  git add -f "$image_path"
+done
